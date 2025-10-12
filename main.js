607a20f03cbadb14032c5b30845d664ab0598ae9
MartioBredge <78837936+MartioBredge@users.noreply.github.com>
2025-10-12 16:42:34 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,23 @@
-console.log("HELLO!");
+async function loadLocalJson() {
+  try {
+    const response = await fetch('matcourses.json'); // Path relative to your HTML file
+    const data = await response.json();
+    console.log(data);
+    // Process your JSON data here
+  } catch (error) {
+    console.error('Error fetching local JSON:', error);
+  }
+}
 
+loadLocalJson();
 
-    async function loadLocalJson() {
-      try {
-        const response = await fetch('matcourses.json'); // Path relative to your HTML file
-        const data = await response.json();
-        console.log(data);
-        // Process your JSON data here
-      } catch (error) {
-        console.error('Error fetching local JSON:', error);
-      }
+class Subject {
+    static allSubjects=[];
+    constructor(jsonData) {
+        this.data=JSON.parse(jsonData);
+        let keys=Object.keys(this.data);
+        for(let i=0;i<keys.length;i++)
+            this[keys[i]]=this.data[keys[i]];
+        Subject.allSubjects.push(this);
     }
-
-    loadLocalJson();
+}
