93a8eaeb7269d88774c3b022f6c21280337140b3
MartioBredge <78837936+MartioBredge@users.noreply.github.com>
2025-10-14 16:52:37 +0800

    Update main.js

--- main.js
+++ main.js
@@ -20,15 +20,23 @@ class Subject {
             this[keys[i]]=this.data[keys[i]];
         Subject.allSubjects.push(this);
     }
+  toString() {
+    return "Course Name: "+this.name+"\n"+"Course Code: "+this.code;
+  }
 }
 
 const courseInput=document.getElementById("courseInput");
 const courseOutput=document.getElementById("courseOutput");
 courseInput.oninput=function(event) {
   let courseName=courseInput.value;
-  console.log(courseName);
   let matches=[];
   for(let i=0;i<Subject.allSubjects.length;i++) {
-    //courseName
+    let subj=Subject.allSubjects[i];
+    if(!subj["name"].includes(courseName)) continue;
+    matches.push(subj);
+  }
+  courseOutput.innerHTML="";
+  for(let i=0;i<matches.length;i++) {
+    courseOutput.innerHTML+=matches[i].toString()+"\n";
   }
 }
