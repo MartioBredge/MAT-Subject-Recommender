8873608fed0ef2863e529c7020edb9fe2492feaa
MartioBredge <78837936+MartioBredge@users.noreply.github.com>
2025-11-12 21:58:51 +0800

    Update main.js

--- main.js
+++ main.js
@@ -31,7 +31,7 @@ function updateRecommendations() {
             let ulist=document.getElementById("y"+i+"s"+j+"Recomm");
             ulist.innerHTML="";
             let label=document.getElementById("y"+i+"s"+j+"RecommLabel");
-            label.innerHTML="Year "+i+" Semester "+j+"<br>"+months[offset];
+            label.innerHTML="Year "+i+" Semester "+j+"<br>"+months[(offset+j)%3];
         }
     }
     
