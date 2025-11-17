c93b00c0922a40abf472d01fd5023aef6b3abf03
MartioBredge <78837936+MartioBredge@users.noreply.github.com>
2025-11-18 06:12:19 +0800

    Update main.js

--- main.js
+++ main.js
@@ -13,6 +13,7 @@ else currSemPickerMonth.value="09";
 currSemPickerYear.value=currentDate.getFullYear().toString();
 
 console.log("WCEWEE");
+console.log("ececrec");
 function createSubjectBox(subject) {
     let mainDiv=document.createElement("div");
     let courseName=document.createElement("h5");
