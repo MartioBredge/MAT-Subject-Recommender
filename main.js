32bcace98b6d82ef6b1bc110d40ff20b356ddc97
MartioBredge <78837936+MartioBredge@users.noreply.github.com>
2025-11-18 09:43:02 +0800

    Update main.js

--- main.js
+++ main.js
@@ -12,8 +12,7 @@ else if(monthIndex>=1) currSemPickerMonth.value="02";
 else currSemPickerMonth.value="09";
 currSemPickerYear.value=currentDate.getFullYear().toString();
 
-console.log("AWCasdWE");
-console.log("WECWE");
+console.log("WEsdfverCWE");
 
 function createSubjectBox(subject) {
     let mainDiv=document.createElement("div");
